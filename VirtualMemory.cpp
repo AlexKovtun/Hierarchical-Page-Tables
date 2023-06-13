269b1b010ddeba26209f6743b946112ef9ee136b
AlexKovtun <alexkov.kovtun@gmail.com>
2023-06-14 00:11:32 +0300

    seems that slides stage work

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -6,7 +6,7 @@
 #include "PhysicalMemory.h"
 #include <cmath>
 
-uint64_t translateAddress (uint64_t virtualAddress, uint64_t *prevFrame);
+uint64_t translateAddress (uint64_t virtualAddress);
 
 /*
  * According to this design, VMInitialize() only has to clear frame 0.
@@ -48,27 +48,12 @@ uint64_t getOffSet (uint64_t virtualAddress)
   return virtualAddress & t;
 }
 
-bool isFrameEmpty (uint64_t baseIndex)
-{
-  uint64_t address = baseIndex * PAGE_SIZE;
-  word_t val = 0;
-  for (int row = 0; row < PAGE_SIZE; ++row)
-    {
-      PMread (address + row, &val);
-      if (val != 0)
-        {
-          return false;
-        }
-    }
-  return true;
-}
-
 /**
  * recursive DFS search
  */
 uint64_t
 traverseTree (int currentDepth, uint64_t currentFrame, uint64_t lastFrame, uint64_t *nextAvailableFrame,
-              uint64_t prevAddress, uint64_t *maxDistance, uint64_t *to_evict, uint64_t *maxParent,
+              uint64_t prevAddress, uint64_t *maxDistance, uint64_t *to_evict, uint64_t *maxParent, uint64_t pathToLeaf,
               uint64_t virtualAddress)
 {
   if (currentFrame > *nextAvailableFrame)
@@ -76,11 +61,11 @@ traverseTree (int currentDepth, uint64_t currentFrame, uint64_t lastFrame, uint6
 
   if (currentDepth == TABLES_DEPTH)
     {
-      uint64_t cyclic_distance = cyclicDistance (currentFrame, virtualAddress);
+      uint64_t cyclic_distance = cyclicDistance (pathToLeaf, virtualAddress);
       if (*maxDistance < cyclic_distance)
         {
           *maxDistance = cyclic_distance;
-          *to_evict = currentFrame;
+          *to_evict = pathToLeaf;
           *maxParent = prevAddress;
         }
       return -1;
@@ -102,6 +87,7 @@ traverseTree (int currentDepth, uint64_t currentFrame, uint64_t lastFrame, uint6
                                        + row, maxDistance,
                                        to_evict,
                                        maxParent,
+                                       (pathToLeaf << OFFSET_WIDTH) + row,
                                        virtualAddress);
           if (ret_val != -1)
             return ret_val;
@@ -119,10 +105,12 @@ traverseTree (int currentDepth, uint64_t currentFrame, uint64_t lastFrame, uint6
 uint64_t findFreeFrame (uint64_t virtualAddress, uint64_t lastFrame)
 {
   uint64_t nextAvailableFrame = 0, maxDistance = 0, to_evict = 0, maxParent = 0;
+  uint64_t pathToLeaf = 0;
   uint64_t address = traverseTree (0, 0, lastFrame,
                                    &nextAvailableFrame,
                                    0,
                                    &maxDistance, &to_evict, &maxParent,
+                                   pathToLeaf,
                                    virtualAddress);
   if (address != -1)
     {
@@ -135,38 +123,36 @@ uint64_t findFreeFrame (uint64_t virtualAddress, uint64_t lastFrame)
       return nextAvailableFrame + 1;
     }
 
-  uint64_t prevFrame = 0;
-  uint64_t tmp = translateAddress (to_evict, &prevFrame);
+  uint64_t tmp = translateAddress (to_evict);
   PMevict (tmp, to_evict);
   clearTable (tmp);
   PMwrite (maxParent, 0);
   return tmp;
 }
 
-uint64_t translateAddress (uint64_t virtualAddress, uint64_t *prevFrame)
+uint64_t translateAddress (uint64_t virtualAddress)
 {
-  uint64_t offSet = getOffSet (virtualAddress);
-  virtualAddress = virtualAddress >> OFFSET_WIDTH; // TODO: is needed?
-  uint64_t currentOffSet = 0, lastFrame = 0;//last frame needed not to corrupt the last father table
+  //virtualAddress = virtualAddress >> OFFSET_WIDTH; // TODO: is needed? NOPE STUPID BUG!
+  uint64_t lastFrame = 0;//last frame needed not to corrupt the last father table
+  uint64_t prevFrame = 0;
   word_t nextToRead = 0;
   bool needToBeRestored = false;
 
   //TODO: this should be generic
   for (int currentDepth = 0; currentDepth < TABLES_DEPTH; ++currentDepth)
     {
-      currentOffSet = (uint64_t)
-                          (virtualAddress >> OFFSET_WIDTH
-                                             * (TABLES_DEPTH - currentDepth
-                                                - 1))
-                      & (PAGE_SIZE - 1);
-      *prevFrame = PAGE_SIZE * nextToRead + currentOffSet;
-      PMread (*prevFrame, &nextToRead);
+      auto currentOffSet = (uint64_t) (
+          virtualAddress >> (OFFSET_WIDTH * (TABLES_DEPTH - currentDepth - 1))
+          &
+          (PAGE_SIZE - 1));
+      prevFrame = PAGE_SIZE * nextToRead + currentOffSet;
+      PMread (prevFrame, &nextToRead);
 
       if (!nextToRead)
         { // we enter here iff we can't find empty frame
-          //uint64_t oldPage = nextToRead;
-          nextToRead = (word_t) findFreeFrame (virtualAddress, lastFrame);
-          PMwrite (*prevFrame, nextToRead);
+          nextToRead = (word_t) findFreeFrame (
+              virtualAddress, lastFrame);
+          PMwrite (prevFrame, nextToRead);
           lastFrame = nextToRead;
           needToBeRestored = true;
         }
@@ -174,7 +160,8 @@ uint64_t translateAddress (uint64_t virtualAddress, uint64_t *prevFrame)
     }
   if (needToBeRestored)
     PMrestore (nextToRead, virtualAddress);
-  return nextToRead * PAGE_SIZE + offSet;
+
+  return nextToRead;
 }
 
 /* Reads a word from the given virtual address
@@ -186,9 +173,14 @@ uint64_t translateAddress (uint64_t virtualAddress, uint64_t *prevFrame)
  */
 int VMread (uint64_t virtualAddress, word_t *value)
 {
-  uint64_t prevFrame = 0;
-  auto readAdd = translateAddress (virtualAddress, &prevFrame);
-  PMread (readAdd, value);
+  if (virtualAddress >= VIRTUAL_MEMORY_SIZE)
+    {
+      return 0;
+    }
+  uint64_t offSet = getOffSet (virtualAddress);
+  auto page = translateAddress (virtualAddress>>OFFSET_WIDTH);
+  auto address = page * PAGE_SIZE + offSet;
+  PMread (address, value);
   return 1;
 }
 
@@ -200,9 +192,14 @@ int VMread (uint64_t virtualAddress, word_t *value)
  */
 int VMwrite (uint64_t virtualAddress, word_t value)
 {
-  uint64_t prevFrame = 0;
-  auto readAdd = translateAddress (virtualAddress, &prevFrame);
-  PMwrite (readAdd, value);
+  if (virtualAddress >= VIRTUAL_MEMORY_SIZE)
+    {
+      return 0;
+    }
+  uint64_t offSet = getOffSet (virtualAddress);
+  auto page = translateAddress (virtualAddress>>OFFSET_WIDTH);
+  auto address = page * PAGE_SIZE + offSet;
+  PMwrite (address, value);
   return 1;
 }
 
